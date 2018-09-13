861cd6485d471a0ff7222b49b70ada098743dfda
Bryan Leighton <bryan.leigh77@gmail.com>
2018-09-13 11:46:32 +0200

    add images for device type to project

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,6 +3,7 @@ class DevicesController < ApplicationController
 		@device_hash = Device.most_popular_devices
 		@day_array= Device.date_table
 		@compare = Device.week_comparison
+		# connect device hash to device model instances
 		@selected_devices = Device.device_select
 		raise
 	end
