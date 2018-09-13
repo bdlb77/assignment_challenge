58650b127234a1452db829faedb872c1d095a378
Bryan Leighton <bryan.leigh77@gmail.com>
2018-09-13 11:45:04 +0200

    connect device ids to device instance models

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,8 +1,10 @@
 class DevicesController < ApplicationController
 	def index
-		@device_array = Device.most_popular_devices
+		@device_hash = Device.most_popular_devices
 		@day_array= Device.date_table
 		@compare = Device.week_comparison
+		@selected_devices = Device.device_select
+		raise
 	end
 
 	def show
--- app/models/device.rb
+++ app/models/device.rb
@@ -9,21 +9,21 @@ class Device < ActiveRecord::Base
 	def self.most_popular_devices
 		hash_new = {}
 		# !!!! HARD CODED THE DAY!!!!!!
-		day_select(11).each do  |result| 
-			if !hash_new.include?(result['id'])
+		day_select(10).each do  |result| 
+			if !hash_new.has_key?(result['id'])
 				hash_new[result['id']] = 1
 			else 
 				hash_new[result['id']] += 1
 			end
 		end
-		hash_new = hash_new.sort_by { |k, v| -v }.first(10).to_h
+		hash_new = hash_new.sort_by { |k, v| -k }.first(10).to_h
 	end
 
 	def self.week_comparison
 		hash_new = most_popular_devices
 		hash_old = {}
-		
-		day_select(11 - 7).each do  |result| 
+
+		day_select(10-7).each do  |result| 
 				if !hash_old.has_key?(result['id'])
 					hash_old[result['id']] = 1
 				else 
@@ -42,14 +42,37 @@ class Device < ActiveRecord::Base
 				end
 			end 
 			# sort old hash values by descending order
-		sorted_old_hash = sorted_old_hash.sort_by { |k, v| -v }.first(10).to_h
-		raise
+		sorted_old_hash = sorted_old_hash.sort_by { |k, v| -k }.first(10).to_h
+		percentage_change_array = []
+		sorted_old_hash.each do |old_key, old_value|
+			hash_new.each do |new_key, new_value|
+				if old_key == new_key
+					# round up to 2nd decimal and reconvert back to float from string
+					percentage_change_array << sprintf('%.2f',((new_value.to_f - old_value.to_f) / old_value.to_f * 100)).to_f
+				end
+			end
+		end
+		percentage_change_array
 	end
 
+	# def self.calculate_percentage_change(new_value, original_value)
+	# 	value_difference = new_value.to_f - original_value.to_f
+	# 	percentage_change = (value_difference.to_f / original_value.to_f * 100)
+	# end
 
 	def self.day_select(input)
 		## Find devices of specific day
 		Device.select{ |device| device.timestamp.day == input}
+	end
+
+	def self.device_select
+		array = []
+		hash_of_device_ids = most_popular_devices
+		hash_of_device_ids.each do |id, occurence|
+			# receive just device instance from  hash of device ids
+			array << Device.where(id: id).uniq{|device| device.id}
+		end
+		array
 	end
 
 
