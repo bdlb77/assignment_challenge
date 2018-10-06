291b19413519536c096de65e48a4cd75e81536bc
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-06 11:08:39 -0700

    added seeds to test edge cases

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#  Creation of devices to check use-cases and edge cases
+Device.create!(timestamp: '2017-04-30T00:00:10Z', id: 'a4a281ad', type: 'sensor', status: 'offline')
+Device.create!(timestamp: '2017-04-30T00:00:10Z', id: 'a4a281ad', type: 'sensor', status: 'offline')
+Device.create!(timestamp: '2018-10-6T00:00:10Z', id: 'a4a281ad', type: 'sensor', status: 'offline')
+Device.create!(timestamp: '2017-10-4T00:00:10Z', id: 'a4a281ad', type: 'sensor', status: 'offline')
+Device.create!(timestamp: '2017-04-30T00:00:10Z', id: 'c702be7d', type: 'gateway', status: 'offline')
+Device.create!(timestamp: '2017-04-30T00:00:10Z', id: 'c702be7d', type: 'gateway', status: 'offline')
