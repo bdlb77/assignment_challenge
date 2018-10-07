e95f7d675ddd740e8dacda411f198e37dc1fba3e
Bryan Leighton <bryan.leigh77@gmail.com>
2018-10-07 16:23:57 +0200

    csv file path in job

--- app/jobs/load_devices_job.rb
+++ app/jobs/load_devices_job.rb
@@ -2,15 +2,13 @@ class LoadDevicesJob < ApplicationJob
   queue_as :default
   def perform(*_args)
     # Do something later
-    puts "seeing if this works.."
     require 'csv'
     Device.destroy_all
-    csv_devices = File.read('/Users/bryanleighton/code/bdlb77/kiwi-assignment/Assignment4/report.csv')
+    csv_devices = File.read('lib/assets/report.csv')
     csv = CSV.parse(csv_devices, headers: true)
     csv.each do |row|
       Device.create!(row.to_hash)
-      puts "Device created"
     end
-    puts "yay"
+    puts "Devices Created"
   end
 end
